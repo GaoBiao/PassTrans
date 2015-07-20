844f436991b6799653723b69f21cc8062e8377be
GaoBiao <gaobiao7912429@gmail.com>
2015-07-20 15:47:24 +0800

    Add get version Url

---
+++ src/com/goo/passtrans/servlet/VersionServlet.java
@@ -0,0 +1,28 @@
+package com.goo.passtrans.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+public class VersionServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = request.getContextPath();
+		String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
+		JSONObject json = new JSONObject();
+		json.put("version", 2);
+		json.put("upgradeUrl", basePath + "static/app/passsave.apk");
+		response.getWriter().print(json);
+	}
+}
